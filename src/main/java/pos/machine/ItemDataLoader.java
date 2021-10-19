585ab95e6e8d3704b70a8118c18a87ba1ece8595
Sean Kramer <sean@kramerdesigns.app>
2021-10-19 18:49:20 +0800

    initial commit

---
+++ src/main/java/pos/machine/ItemDataLoader.java
@@ -0,0 +1,28 @@
+package pos.machine;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemDataLoader {
+  public static List<Item> loadItems() {
+    return Arrays.asList(
+      new Item("ITEM000000", "Coca-Cola", 3),
+      new Item("ITEM000001", "Sprite", 3),
+      new Item("ITEM000004", "Battery", 2)
+    );
+  }
+
+  public static List<String> loadBarcodes() {
+    return Arrays.asList(
+      "ITEM000000",
+      "ITEM000000",
+      "ITEM000000",
+      "ITEM000000",
+      "ITEM000001",
+      "ITEM000001",
+      "ITEM000004",
+      "ITEM000004",
+      "ITEM000004"
+    );
+  }
+}
